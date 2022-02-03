8a81e0d19703b57bc3e10a14893dfb89d7def52a
Malin Nilsson <malinnilsson87@gmail.com>
2022-02-03 19:45:17 +0100

    Setting up

---
+++ data/todos.js
@@ -0,0 +1,27 @@
+const todos = [{
+    id: 1,
+    created: "2022-01-20 13:20:20",
+    description: "Do yoga",
+    done: false
+},
+{
+    id: 2,
+    created:  "2022-01-21 20:02:01",
+    description: "Eat some vegetables",
+    done: false
+},
+{
+    id: 3,
+    created:  "2022-01-22 09:16:46",
+    description: "Watch Ozark",
+    done: false
+},
+{
+    id: 3,
+    created:  "2022-01-23 12:20:46",
+    description: "Pick up package",
+    done: false
+},
+]
+
+module.exports = todos;
